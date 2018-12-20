8def4c6a170c1e0981e4f79424b7bdcd2363b755
Farangi <elfarangi@gmail.com>
2018-12-20 18:57:10 +0500

    initial Deployment 20/12/18

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZVVH48crS31Hh7croWdHqySpvp5ONp9k',
+    authDomain: 'wat-wonni.firebaseapp.com',
+    databaseURL: 'https://wat-wonni.firebaseio.com',
+    projectId: 'wat-wonni',
+    storageBucket: 'wat-wonni.appspot.com',
+    messagingSenderId: '317478179110'
+  }
 };
